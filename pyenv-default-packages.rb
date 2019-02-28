5b7ef467cbb6c701b31cfb43dcedc32bfc88e8e5
Kyoichiro Yamada <kyoh86@users.noreply.github.com>
2019-02-28 20:14:28 +0900

    Publish pyenv-default-packages@v0.3.2

--- pyenv-default-packages.rb
+++ pyenv-default-packages.rb
@@ -1,8 +1,8 @@
 class PyenvDefaultPackages < Formula
   homepage "https://github.com/kyoh86/pyenv-default-packages"
   head "https://github.com/kyoh86/pyenv-default-packages.git"
-  url "https://github.com/kyoh86/pyenv-default-packages/archive/0.3.1.tar.gz"
-  sha256 "163ea3f5b98aef826d3c6c8d4177c99c006faf87877a30d7828e897c97d5e9cc"
+  url "https://github.com/kyoh86/pyenv-default-packages/archive/v0.3.2.tar.gz"
+  sha256 "d27fb8a3e078aa3168471d586a36d6ed9c41a5a54ec6cfeb6cdd4f0e055312d6"
 
   depends_on "pyenv"
   depends_on "pyenv-virtualenv" => :recommended
